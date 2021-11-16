f4c8988d6a2608879d6fae905b021baf023bc2fb
Alex <alex.duensing@gmail.com>
2021-11-16 22:11:26 +0100

    Minor refactorings for Dao methods

--- app/src/main/java/de/adue/workoutplanner/data/WorkoutPlanDao.kt
+++ app/src/main/java/de/adue/workoutplanner/data/WorkoutPlanDao.kt
@@ -6,32 +6,39 @@ import androidx.room.*
 @Dao
 abstract class WorkoutPlanDao {
     @Insert
-    abstract fun insert(workoutPlan: WorkoutPlan)
+    abstract fun insertWorkoutPlan(workoutPlan: WorkoutPlan)
 
     // TODO: This will require more logic to delete the relationship between workout plans, exercises and so on
     @Delete
     abstract fun delete(workoutPlan: WorkoutPlan)
 
     @Query("SELECT * FROM workoutplan")
-    abstract fun getAll(): LiveData<List<WorkoutPlan>>
+    abstract fun getWorkoutPlans(): LiveData<List<WorkoutPlan>>
+
+    @Query("SELECT * FROM exercise")
+    abstract fun getExercises(): LiveData<List<Exercise>>
 
     @Transaction
     @Query("SELECT * FROM workoutplan")
     abstract fun getWorkoutPlansWithExercises(): List<WorkoutPlanWithSplitsAndExercises>
 
     @Transaction
-    open fun addSplitWithExercises(workoutPlanId: Int, name: String, exercises: List<Exercise>) {
+    open fun insertSplitWithExercises(workoutPlanId: Int, name: String, exercises: List<Exercise>) {
         insertSplit(workoutPlanId, name)
+        val splitId = getLatestSplitId()
         exercises.forEach {
-            insertSplitExercises(0, it.exerciseId) // TODO get splitId from previously inserted split
+            insertSplitExercises(splitId, it.exerciseId)
         }
     }
 
     @Query("INSERT INTO split (workoutPlanId, name) VALUES (:workoutPlanId, :name)")
     abstract fun insertSplit(workoutPlanId: Int, name: String)
 
+    @Query("SELECT MAX(splitId) from split")
+    abstract fun getLatestSplitId(): Int
+
     @Insert
-    abstract fun insertExercise()
+    abstract fun insertExercise(exercise: Exercise)
 
     @Query("INSERT INTO SplitExercisesCrossRef (splitId, exerciseId) VALUES (:splitId, :exerciseId)")
     abstract fun insertSplitExercises(splitId: Int, exerciseId: Int)
