3839e8b0c31f399d28913d3ffcff9f80bc7a373e
Alex <alex.duensing@gmail.com>
2021-11-30 18:00:56 +0100

    Removed test code

--- app/src/main/java/de/adue/workoutplanner/ui/workoutplan/ui/WorkoutPlanFragment.kt
+++ app/src/main/java/de/adue/workoutplanner/ui/workoutplan/ui/WorkoutPlanFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import de.adue.workoutplanner.data.Exercise
 import de.adue.workoutplanner.databinding.FragmentWorkoutPlanBinding
 import de.adue.workoutplanner.ui.workoutplan.WorkoutPlanViewModel
 
@@ -27,36 +26,8 @@ class WorkoutPlanFragment : Fragment() {
 
         _binding = FragmentWorkoutPlanBinding.inflate(inflater, container, false)
 
-        /*
         _binding?.recyclerviewPlans?.apply {
 
-        }
-        */
-
-        _binding?.buttonAddPlan?.setOnClickListener {
-            _binding?.inputTestTable?.text?.toString()?.let {
-                workoutPlanViewModel.insertPlan(it)
-            }
-        }
-        listOf (
-            Exercise(name = "Squats", isBodyweight = false),
-            Exercise(name = "Leg curls", isBodyweight = false),
-            Exercise(name = "Beinbeuger", isBodyweight = false),
-            Exercise(name = "Beinstrecker", isBodyweight = false),
-            Exercise(name = "Bench press", isBodyweight = false),
-            Exercise(name = "High row", isBodyweight = false),
-            Exercise(name = "Lat", isBodyweight = false),
-            Exercise(name = "Shoulder press", isBodyweight = false)
-        ).forEach {
-            workoutPlanViewModel.insertExercise(it)
-        }
-
-        workoutPlanViewModel.exercises.observe(viewLifecycleOwner) {
-            var columns = ""
-            it.forEach { plan ->
-                columns += plan.name + " (${plan.exerciseId})\n"
-            }
-            _binding?.textTestTable?.text = columns
         }
 
         return binding?.root
